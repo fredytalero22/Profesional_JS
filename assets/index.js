37c94764e1625d4ab6e039ac2a977b5259611a98
Fredy Talero <fredy.talero@gmail.com>
2020-10-02 18:30:55 -0500

    this

--- assets/index.js
+++ assets/index.js
@@ -5,6 +5,9 @@ const video = document.querySelector("video");
 const buttonPlay = document.getElementById("play");
 const buttonMute = document.getElementById("mute");
 
-const player = new MediaPlayer({ el: video, plugins: [ new AutoPlay() ] });
+const player = new MediaPlayer({ el: video, plugins: [
+//     new AutoPlay() 
+    ] 
+});
 buttonPlay.onclick = () => video.paused ? player.play() : player.pause();
 buttonMute.onclick = () => video.muted ? player.unmute() : player.mute();
