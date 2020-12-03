4f7f21a6f645ec14d934f10e7bf6870a62cea15e
Fredy Talero <fredy.talero@gmail.com>
2020-12-02 20:46:09 -0500

    VisibilityChange

--- assets/plugins/AutoPause.js
+++ assets/plugins/AutoPause.js
@@ -2,6 +2,7 @@ class AutoPause {
     constructor(){
         this.threshold = 0.25
         this.handleIntersection = this.handleIntersection.bind(this);
+        this.handleVisibilityChange = this.handleVisibilityChange.bind(this);
     }
 
     run(player){
@@ -12,6 +13,8 @@ class AutoPause {
         })
 
         observer.observe(this.player.media)
+
+        document.addEventListener("visibilitychange", this.handleVisibilityChange)
     }
 
     handleIntersection(entries){
@@ -22,7 +25,16 @@ class AutoPause {
         }else{
             this.player.play();
         }
-        console.log(entry);
+    }
+
+    handleVisibilityChange() {
+        const isVisible = document.visibilityState === "visible"
+
+        if(isVisible){
+            this.player.play()
+        }else{
+            this.player.pause();
+        }
     }
 }
 
